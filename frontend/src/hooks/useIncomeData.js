9c0f691f714236ca226fba72e8a032020cd4666c
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-04 14:31:05 +0600

    useIncomeData custom hook added for retrieving incomeData

---
+++ frontend/src/hooks/useIncomeData.js
@@ -0,0 +1,32 @@
+// useIncomeData.js
+import { useEffect } from 'react';
+import IncomeService from '../services/income.service';
+import { useIncomeContext } from '../contexts/income.context';
+
+export function useIncomeData() {
+  const { setIncomeData } = useIncomeContext();
+
+  useEffect(() => {
+    const fetchIncomeData = async () => {
+      try {
+        const response = await IncomeService.getIncome();
+        const fetchedIncomeList = response.data;
+
+        const updatedIncomeList = await Promise.all(
+          fetchedIncomeList.map(async (income) => {
+            const incomeId = income.id;
+            const transactionResponse = await IncomeService.getAllIncomeTransactions(incomeId);
+            const transactions = transactionResponse.data;
+            return { ...income, transactions };
+          })
+        );
+
+        setIncomeData(updatedIncomeList);
+      } catch (error) {
+        console.error('Error fetching income data:', error);
+      }
+    };
+
+    fetchIncomeData();
+  }, [setIncomeData]);
+}
