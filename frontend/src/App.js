dbdf35bdc9fbfddb0b88dfbaf8cee927796a12c6
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-04 06:06:02 +0600

    All the essential React core file initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthService from './services/auth.service';
+import Login from './components/login/login.component';
+import Register from './components/register/register.component';
+import NavBar from './components/navbar/navbar.component';
+import Income from './components/income/income.component';
+import Logout from './components/logout/logout.component';
+import AddIncome from './components/income/add_income.component';
+
+const App = () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    // Redirect to login if token is not present
+    return <Login />;
+  }
+  AuthService.setAuthToken(token);
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/addIncome" element={<AddIncome />} />
+        </Routes>
+      </div>
+    </Router>
+
+  );
+};
+
+export default App;
