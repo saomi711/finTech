0f3b12b8b08a9cbb41d08a724a6fb5020f65fed3
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-06 22:49:37 +0600

    some additional functions added

--- frontend/src/services/income.service.js
+++ frontend/src/services/income.service.js
@@ -15,6 +15,17 @@ const IncomeService = {
       throw error;
     }
   },
+  getIncomeById: async (incomeId) => {
+    try {
+      const token = localStorage.getItem('token');
+      AuthService.setAuthToken(token);
+      const response = await axios.get(`${API_URL}/income/${incomeId}`);
+  
+      return response; // Return the specific income data
+    } catch (error) {
+      throw error;
+    }
+  },
   addIncome: async (incomeData) => {
     try {
       const token = localStorage.getItem('token');
@@ -28,18 +39,43 @@ const IncomeService = {
       throw error;
     }
   },
+  editIncome: async (incomeId, updatedIncomeData) => {
+    try {
+      const token = localStorage.getItem('token');
+      AuthService.setAuthToken(token);
+      const incomeResponse = await axios.put(`${API_URL}/income/${incomeId}`, updatedIncomeData);
+  
+      return incomeResponse;
+    } catch (error) {
+      throw error;
+    }
+  },
   addIncomeTransaction: async (incomeId, transactionData) => {
     try {
       const token = localStorage.getItem('token');
       AuthService.setAuthToken(token);
       const transactionResponse = await axios.post(`${API_URL}/income/${incomeId}/transaction`,transactionData);
 
-      return transactionResponse.data;
+      return transactionResponse;
     } catch (error) {
       throw error;
     }
   },
-  getAllIncomeTransactions: async (incomeId) => {
+  editIncomeTransaction: async (incomeId, transactionId, updatedTransactionData) => {
+    try {
+      const token = localStorage.getItem('token');
+      AuthService.setAuthToken(token);
+      const transactionResponse = await axios.put(
+        `${API_URL}/income/${incomeId}/transaction/${transactionId}`,
+        updatedTransactionData
+      );
+  
+      return transactionResponse;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getIncomeTransactions: async (incomeId) => {
     try {
       const token = localStorage.getItem('token');
       AuthService.setAuthToken(token);
@@ -65,6 +101,21 @@ const IncomeService = {
       throw error;
     }
   },
+  deleteIncomeTransaction: async (incomeId, transactionId) => {
+    try {
+      const token = localStorage.getItem('token');
+      AuthService.setAuthToken(token);
+      const response = await axios.delete(`${API_URL}/income/${incomeId}/transaction/${transactionId}`);
+      if (response.status === 200) {
+        console.log(`Income transaction with ID ${transactionId} has been deleted.`);
+      } else {
+        throw new Error(`Failed to delete income transaction with ID ${transactionId}.`);
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+  
 
 
 };
