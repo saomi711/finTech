143ef2f363d11de03cc1ca697cb60c5d4320149c
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-06 23:49:48 +0600

    Dashboard added

---
+++ frontend/src/components/dashboard/dashboard.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../navbar/navbar.component';
+
+const Dashboard = () => {
+
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to the Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
