30d9e14d7b0f215446cc37b7a0123659e0ff8502
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-13 14:02:18 +0600

    auth context added

---
+++ frontend/src/contexts/auth.context.js
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import AuthService from '../services/auth.service'; // Adjust the path to match your file structure
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+  //const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+        try {
+          const token = localStorage.getItem('token');
+          if (token) {
+            const userResponse = await AuthService.user(token);
+            if (userResponse.data) {
+                setLoggedIn(true);
+                //setUser(userResponse.data);
+            }
+            console.log(userResponse.data);
+            
+          }
+        } catch (error) {
+          console.error('Error checking login status:', error);
+        }
+      };
+  
+      checkLoggedIn();
+  }, []);
+
+  
+
+  return (
+    <AuthContext.Provider value={{ loggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
