76a0d74aa1c06e0b64bde9ef2ff87ef57877c48e
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-13 14:03:36 +0600

    material component added

--- frontend/src/components/login/login.component.js
+++ frontend/src/components/login/login.component.js
@@ -5,13 +5,13 @@ import { UserContext } from '../../contexts/user.context';
 import App from '../../App';
 import NavBar from '../navbar/navbar.component';
 import { Link } from 'react-router-dom';
+import { Button, TextField, Typography, Container, CssBaseline, Grid } from '@material-ui/core';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rerender, setRerender] = useState(false);
   const navigate = useNavigate();
-  const { setUser } = useContext(UserContext);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -19,15 +19,7 @@ const Login = () => {
       const login_response = await AuthService.login(email, password);
       console.log(login_response);
       if (login_response.data.token) {
-        const { id, email, username } = login_response.data.user; // Assuming the API returns user data
-        console.log(username);
-        setUser({
-            id,
-            email,
-            username,
-            token: login_response.data.token,
-        });
-        
+        //setUser(login_response.data.user);
         localStorage.setItem('token', login_response.data.token);
         return navigate(-1);
       }
@@ -38,14 +30,42 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div>
+        <Typography variant="h5">Login</Typography>
+        <form onSubmit={handleLogin}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Login
+          </Button>
+          <Typography align="center">
+            Don't have an account? <Link to="/register">Register</Link>
+          </Typography>
+        </form>
+      </div>
+    </Container>
   );
 };
 
