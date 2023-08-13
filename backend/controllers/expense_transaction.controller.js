0cf261da7053cf1d2563010d4e95b87073ad0e95
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-13 17:56:37 +0600

    expense_transaction controller changed

--- backend/controllers/expense_transaction.controller.js
+++ backend/controllers/expense_transaction.controller.js
@@ -1,8 +1,28 @@
 const ExpenseTransaction = require('../models/expense_transaction.model');
+const Expense = require('../models/expense.model');
+
+const isExpenseBelongsToUser = async (userId, expenseId) => {
+  try {
+    const expense = await Expense.findOne({ where: { id: expenseId, user_id: userId } });
+    return !!expense;
+  } catch (err) {
+    console.error('Error checking expense ownership:', err);
+    return false;
+  }
+};
 
 exports.createExpenseTransaction = async (req, res) => {
+  const expenseId = req.params.expense_id;
+  const userId = req.userId;
+  const isValidExpense = await isExpenseBelongsToUser(userId, expenseId);
+
+  if (!isValidExpense) {
+    return res.status(403).json({ message: 'You are not authorized to create a transaction for this expense.' });
+  }
+
   try {
-    const expenseTransaction = await ExpenseTransaction.create(req.body);
+    const expenseTransactionData = { ...req.body, expense_id: expenseId };
+    const expenseTransaction = await ExpenseTransaction.create(expenseTransactionData);
     res.status(201).json(expenseTransaction);
   } catch (err) {
     console.error('Error creating expense transaction:', err);
@@ -11,8 +31,16 @@ exports.createExpenseTransaction = async (req, res) => {
 };
 
 exports.getAllExpenseTransactions = async (req, res) => {
+  const expenseId = req.params.expense_id;
+  const userId = req.userId;
+  const isValidExpense = await isExpenseBelongsToUser(userId, expenseId);
+
+  if (!isValidExpense) {
+    return res.status(403).json({ message: 'You are not authorized to get transactions for this expense.' });
+  }
+
   try {
-    const expenseTransactions = await ExpenseTransaction.findAll();
+    const expenseTransactions = await ExpenseTransaction.findAll({ where: { expense_id: expenseId } });
     res.json(expenseTransactions);
   } catch (err) {
     console.error('Error getting expense transactions:', err);
@@ -21,9 +49,19 @@ exports.getAllExpenseTransactions = async (req, res) => {
 };
 
 exports.getExpenseTransactionById = async (req, res) => {
-  const transactionId = req.params.id;
+  const expenseId = req.params.expense_id;
+  const expenseTransactionId = req.params.id;
+  const userId = req.userId;
+  const isValidExpense = await isExpenseBelongsToUser(userId, expenseId);
+
+  if (!isValidExpense) {
+    return res.status(403).json({ message: 'You are not authorized to get this transaction.' });
+  }
+
   try {
-    const expenseTransaction = await ExpenseTransaction.findByPk(transactionId);
+    const expenseTransaction = await ExpenseTransaction.findOne({
+      where: { id: expenseTransactionId, expense_id: expenseId }
+    });
     if (!expenseTransaction) {
       return res.status(404).json({ message: 'Expense transaction not found' });
     }
@@ -35,10 +73,18 @@ exports.getExpenseTransactionById = async (req, res) => {
 };
 
 exports.updateExpenseTransaction = async (req, res) => {
-  const transactionId = req.params.id;
+  const expenseId = req.params.expense_id;
+  const expenseTransactionId = req.params.id;
+  const userId = req.userId; 
+  const isValidExpense = await isExpenseBelongsToUser(userId, expenseId);
+
+  if (!isValidExpense) {
+    return res.status(403).json({ message: 'You are not authorized to update this transaction.' });
+  }
+
   try {
     const [updatedRowsCount, updatedRows] = await ExpenseTransaction.update(req.body, {
-      where: { id: transactionId },
+      where: { id: expenseTransactionId, expense_id: expenseId },
       returning: true,
     });
     if (updatedRowsCount === 0) {
@@ -52,15 +98,25 @@ exports.updateExpenseTransaction = async (req, res) => {
 };
 
 exports.deleteExpenseTransaction = async (req, res) => {
-  const transactionId = req.params.id;
+  const expenseId = req.params.expense_id;
+  const expenseTransactionId = req.params.id;
+  const userId = req.userId; 
+  const isValidExpense = await isExpenseBelongsToUser(userId, expenseId);
+
+  if (!isValidExpense) {
+    return res.status(403).json({ message: 'You are not authorized to delete this transaction.' });
+  }
+
   try {
-    const deletedRowsCount = await ExpenseTransaction.destroy({ where: { id: transactionId } });
+    const deletedRowsCount = await ExpenseTransaction.destroy({
+      where: { id: expenseTransactionId, expense_id: expenseId }
+    });
     if (deletedRowsCount === 0) {
       return res.status(404).json({ message: 'Expense transaction not found' });
     }
     res.json({ message: 'Expense transaction deleted successfully' });
   } catch (err) {
-    console.error('Error deleting expense transaction:', err);
+    console.error('Error deleting income transaction:', err);
     res.status(500).json({ message: 'Server error' });
   }
 };
