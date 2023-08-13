aa0a4b4370be9c000fc1ae0ddf455e7214241c23
saomi711 <1705022@ugrad.cse.buet.ac.bd>
2023-08-13 17:52:58 +0600

    expense route added to the navbar

--- frontend/src/components/navbar/navbar.component.js
+++ frontend/src/components/navbar/navbar.component.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { useAuth } from '../../contexts/auth.context';
 
-const NavBar = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'inherit',
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const Navigation = () => {
+  const classes = useStyles();
+  const { loggedIn,user } = useAuth();
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/income">Income</Link>
-        </li>
-        <li>
-          <Link to="/addIncome">Add Income</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            finTech
+          </Typography>
+          <Button component={Link} to="/" className={classes.link}>
+            Home
+          </Button>
+          
+          {!loggedIn ? (
+            <>
+              <Button component={Link} to="/login" className={classes.link}>
+                Login
+              </Button>
+              <Button component={Link} to="/register" className={classes.link}>
+                Register
+              </Button>
+            </>
+          ) : null}
+          {loggedIn ? (
+            <>
+              <Button component={Link} to="/income" className={classes.link}>
+                Income
+              </Button>
+              <Button component={Link} to="/expense" className={classes.link}>
+                Expense
+              </Button>
+              <Button component={Link} to="/logout" className={classes.link}>
+                Logout
+              </Button>
+            </>
+          ) : null}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
-export default NavBar;
+export default Navigation;
